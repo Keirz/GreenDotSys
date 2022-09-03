2161d68db74f1d4638b520b32f6f9049f9c4d89d
rickrribeiro <rickribeiro99@gmail.com>
2022-09-03 20:31:38 -0300

    feat: node structure before apply ejs

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express');
+const app = express();
+const path = require('path')
+const { login, teste } = require('./query')
+
+app.use(express.json({}));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', require('ejs-locals'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res)=>{
+  res.render('example', {games: [{name:"AAA", description:"BBB"}]})
+})
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const auth = login(username, password)
+  return auth
+})
+
+app.post('/survey', (req, res) => {
+  console.log(req.body)
+  const { interests, degree, workHours, profile } = req.body
+  console.log(interests)
+  console.log(workHours)
+  res.status(200).json({mock:"meu ovo"})
+})
+
+
+app.listen(3000, ()=>{
+  console.log("O app está rodando na porta 3000!")
+})
